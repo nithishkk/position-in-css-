d66af99d17c8871c53938b7eb0f153a04cceb0b5
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-05-06 11:56:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# position-in-css-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-noxbrt)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="parent">
+     <div className="child">nithish</div>
+     <div className='parent1'> 
+     <div className='child1'>react js</div>
+     
+     </div>
+    </div>
+  );
+}
